0eb99847f0626eace980e90649a31192af9dbea8
soyaibur <ovisoyaiburrahman@gmail.com>
2021-12-29 15:04:24 +0600

    function for starting game added

--- logic.js
+++ logic.js
@@ -1,19 +1,31 @@
 // ....Univesal declaration and others......
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-
-
 // .....All Element Selection will be undernith here...........
+const startButton = document.getElementById('start-btn')
+const questionContainer = document.getElementById('question-container')
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
-
 // .....All EventListener go undernith here...........
+startButton.addEventListener('click',()=>{
+  startGame()
+})
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 
 // ......All Function will go here undernith..........
+function startGame(){
+  startButton.classList.add('hide')
+  questionContainer.classList.remove('hide')
+}
+
+function setNextQuestion(){
+
+}
+function selectAnswer(){
+
+}
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 //All code will be deleted here.............
